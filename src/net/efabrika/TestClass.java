2b0e3d0b57d3745bf157c147711803636ca00c30
Hami Torun <hamitorun@gmail.com>
2014-11-02 16:43:39 +0200

    Add TestClass

---
+++ src/net/efabrika/TestClass.java
@@ -0,0 +1,22 @@
+package net.efabrika;
+
+/**
+ * Description
+ *
+ * @author Hami Torun [hamitorun (at) gmail.com]
+ */
+public class TestClass {
+    public int myInt;
+
+    public TestClass() {
+
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public void setMyInt(int i) {
+        myInt = i;
+    }
+}
