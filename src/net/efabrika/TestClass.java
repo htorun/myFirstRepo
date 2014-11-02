411911c123eaa227aa485fe10979d5257d0e202b
Hami Torun <hamitorun@gmail.com>
2014-11-02 16:43:39 +0200

    Add TestClass with javadoc @version %I%, %G%

---
+++ src/net/efabrika/TestClass.java
@@ -0,0 +1,23 @@
+package net.efabrika;
+
+/**
+ * Description
+ *
+ * @version %I%, %G%
+ * @author Hami Torun
+ */
+public class TestClass {
+    public int myInt;
+
+    public TestClass() {
+
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public void setMyInt(int i) {
+        myInt = i;
+    }
+}
